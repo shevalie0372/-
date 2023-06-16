7f8e32da7cd593e1749cc0b9fa7e37cc1fe55878
shevalie0372 <136812493+shevalie0372@users.noreply.github.com>
2023-06-16 14:53:28 +0300

    Add files via upload

---
+++ ПР10.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace пр10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Scroll_Red = new System.Windows.Forms.HScrollBar();
+            this.Scroll_Green = new System.Windows.Forms.HScrollBar();
+            this.Scroll_Blue = new System.Windows.Forms.HScrollBar();
+            this.numeric_Red = new System.Windows.Forms.NumericUpDown();
+            this.numeric_Green = new System.Windows.Forms.NumericUpDown();
+            this.numeric_Blue = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.picResultColor = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_Red)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_Green)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_Blue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picResultColor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Scroll_Red
+            // 
+            this.Scroll_Red.LargeChange = 1;
+            this.Scroll_Red.Location = new System.Drawing.Point(54, 109);
+            this.Scroll_Red.Maximum = 255;
+            this.Scroll_Red.Name = "Scroll_Red";
+            this.Scroll_Red.Size = new System.Drawing.Size(328, 17);
+            this.Scroll_Red.TabIndex = 0;
+            this.Scroll_Red.Scroll += new System.Windows.Forms.ScrollEventHandler(this.Scroll_Red_Scroll);
+            this.Scroll_Red.ValueChanged += new System.EventHandler(this.Scroll_Red_ValueChanged);
+            // 
+            // Scroll_Green
+            // 
+            this.Scroll_Green.LargeChange = 1;
+            this.Scroll_Green.Location = new System.Drawing.Point(54, 140);
+            this.Scroll_Green.Maximum = 255;
+            this.Scroll_Green.Name = "Scroll_Green";
+            this.Scroll_Green.Size = new System.Drawing.Size(328, 17);
+            this.Scroll_Green.TabIndex = 1;
+            this.Scroll_Green.Scroll += new System.Windows.Forms.ScrollEventHandler(this.Scroll_Green_Scroll);
+            // 
+            // Scroll_Blue
+            // 
+            this.Scroll_Blue.LargeChange = 1;
+            this.Scroll_Blue.Location = new System.Drawing.Point(54, 167);
+            this.Scroll_Blue.Maximum = 255;
+            this.Scroll_Blue.Name = "Scroll_Blue";
+            this.Scroll_Blue.Size = new System.Drawing.Size(328, 17);
+            this.Scroll_Blue.TabIndex = 2;
+            this.Scroll_Blue.Scroll += new System.Windows.Forms.ScrollEventHandler(this.Scroll_Blue_Scroll);
+            // 
+            // numeric_Red
+            // 
+            this.numeric_Red.Location = new System.Drawing.Point(385, 109);
+            this.numeric_Red.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numeric_Red.Name = "numeric_Red";
+            this.numeric_Red.Size = new System.Drawing.Size(52, 20);
+            this.numeric_Red.TabIndex = 3;
+            this.numeric_Red.ValueChanged += new System.EventHandler(this.numeric_Red_ValueChanged);
+            // 
+            // numeric_Green
+            // 
+            this.numeric_Green.Location = new System.Drawing.Point(385, 140);
+            this.numeric_Green.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numeric_Green.Name = "numeric_Green";
+            this.numeric_Green.Size = new System.Drawing.Size(52, 20);
+            this.numeric_Green.TabIndex = 4;
+            this.numeric_Green.ValueChanged += new System.EventHandler(this.numeric_Green_ValueChanged);
+            // 
+            // numeric_Blue
+            // 
+            this.numeric_Blue.Location = new System.Drawing.Point(385, 167);
+            this.numeric_Blue.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numeric_Blue.Name = "numeric_Blue";
+            this.numeric_Blue.Size = new System.Drawing.Size(52, 20);
+            this.numeric_Blue.TabIndex = 5;
+            this.numeric_Blue.ValueChanged += new System.EventHandler(this.numeric_Blue_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Red";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Green";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Blue";
+            // 
+            // picResultColor
+            // 
+            this.picResultColor.Location = new System.Drawing.Point(444, 109);
+            this.picResultColor.Name = "picResultColor";
+            this.picResultColor.Size = new System.Drawing.Size(109, 78);
+            this.picResultColor.TabIndex = 9;
+            this.picResultColor.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 264);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(170, 264);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(460, 264);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Other colors";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 315);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picResultColor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numeric_Blue);
+            this.Controls.Add(this.numeric_Green);
+            this.Controls.Add(this.numeric_Red);
+            this.Controls.Add(this.Scroll_Blue);
+            this.Controls.Add(this.Scroll_Green);
+            this.Controls.Add(this.Scroll_Red);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_Red)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_Green)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_Blue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picResultColor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.HScrollBar Scroll_Red;
+        private System.Windows.Forms.HScrollBar Scroll_Green;
+        private System.Windows.Forms.HScrollBar Scroll_Blue;
+        private System.Windows.Forms.NumericUpDown numeric_Red;
+        private System.Windows.Forms.NumericUpDown numeric_Green;
+        private System.Windows.Forms.NumericUpDown numeric_Blue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox picResultColor;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ПР10.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace пр10
+{
+    public partial class Form1 : Form
+    {
+        Color colorResult;
+        public Form1()
+        {
+            InitializeComponent();          
+        }
+        public Form1(Color color)
+        {
+            InitializeComponent();
+            Scroll_Red.Tag = numeric_Red;
+            Scroll_Green.Tag = numeric_Green;
+            Scroll_Blue.Tag = numeric_Blue;
+
+            numeric_Red.Tag = Scroll_Red;
+            numeric_Green.Tag = Scroll_Green;
+            numeric_Blue.Tag = Scroll_Blue;
+
+            numeric_Red.Value = color.R;
+            numeric_Green.Value = color.G;
+            numeric_Blue.Value = color.B;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                Scroll_Red.Tag = colorDialog.Color.R;
+                Scroll_Green.Tag = colorDialog.Color.G;
+                Scroll_Blue.Tag = colorDialog.Color.B;
+
+                colorResult = colorDialog.Color;
+                UpdateColor();
+            }
+        }
+        
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Scroll_Red_Scroll(object sender, ScrollEventArgs e)
+        {
+          
+        }
+
+        private void numeric_Red_ValueChanged(object sender, EventArgs e)
+        {
+            NumericUpDown numericUpDown = (NumericUpDown)sender;
+            ScrollBar scrollBar = (ScrollBar)numericUpDown.Tag;
+            scrollBar.Value = (int)numericUpDown.Value;
+        }
+
+        private void Scroll_Green_Scroll(object sender, ScrollEventArgs e)
+        {
+            ScrollBar scrollBar = (ScrollBar)sender;
+            NumericUpDown numericUpDown = (NumericUpDown)scrollBar.Tag;
+            numericUpDown.Value = scrollBar.Value;
+        }
+
+        private void numeric_Green_ValueChanged(object sender, EventArgs e)
+        {
+            NumericUpDown numericUpDown = (NumericUpDown)sender;
+            ScrollBar scrollBar = (ScrollBar)numericUpDown.Tag;
+            scrollBar.Value = (int)numericUpDown.Value;
+        }
+
+        private void Scroll_Blue_Scroll(object sender, ScrollEventArgs e)
+        {
+            ScrollBar scrollBar = (ScrollBar)sender;
+            NumericUpDown numericUpDown = (NumericUpDown)scrollBar.Tag;
+            numericUpDown.Value = scrollBar.Value;
+        }
+
+        private void numeric_Blue_ValueChanged(object sender, EventArgs e)
+        {
+            NumericUpDown numericUpDown = (NumericUpDown)sender;
+            ScrollBar scrollBar = (ScrollBar)numericUpDown.Tag;
+            scrollBar.Value = (int)numericUpDown.Value;
+        }
+        private void UpdateColor()
+        {
+            colorResult = Color.FromArgb(Scroll_Red.Value, Scroll_Green.Value, Scroll_Blue.Value);
+            picResultColor.BackColor = colorResult;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+
+        private void Scroll_Red_ValueChanged(object sender, EventArgs e)
+        {
+            ScrollBar scrollBar = (ScrollBar)sender;
+            NumericUpDown numericUpDown = (NumericUpDown)scrollBar.Tag;
+            Scroll_Red.Value = scrollBar.Value;
+        }
+    }
+}
